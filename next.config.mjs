47e537c2bd31f643f2e235da38af6ec5e94ea5e9
Lucas Dickey <lucas@lucasdickey.com>
2024-11-02 11:59:27 -0700

    more config changes

---
+++ next.config.mjs
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  experimental: {
+    react: {
+      version: "18.2.0"
+    }
+  }
+};
+
+export default nextConfig; 
