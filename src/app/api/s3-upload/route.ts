4a1abcee4cd861d7b195274ede26244ca365ea50
Lucas Dickey <lucas@lucasdickey.com>
2024-11-05 20:41:51 -0800

    change agian to the routesegment

--- src/app/api/s3-upload/route.ts
+++ src/app/api/s3-upload/route.ts
@@ -2,8 +2,10 @@ import { NextResponse } from 'next/server'
 import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3'
 import { v4 as uuidv4 } from 'uuid'
 
-export const runtime = 'nodejs'
-export const dynamic = 'force-dynamic'
+export const routeSegmentConfig = {
+  runtime: 'nodejs',
+  dynamic: 'force-dynamic',
+}
 
 const s3Client = new S3Client({
   region: process.env.AWS_REGION!,
