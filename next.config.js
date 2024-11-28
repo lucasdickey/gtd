4db721760a019a1501f3a5aa25a51af95815fa6a
Lucas Dickey <lucas@lucasdickey.com>
2024-11-28 12:08:21 -0800

    fix favicon/etc icons
    wanted to get desktop launcher on Android working properly

--- next.config.js
+++ next.config.js
@@ -18,6 +18,19 @@ const nextConfig = {
       },
     ]
   },
+  async headers() {
+    return [
+      {
+        source: '/:all*(svg|jpg|png)',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=31536000, immutable',
+          },
+        ],
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
