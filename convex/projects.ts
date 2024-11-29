c56253220d588353da7ec78720bfaeae2c6c749a
Lucas Dickey <lucas@lucasdickey.com>
2024-11-29 11:58:19 -0800

    merge conflict on projects schema

--- convex/projects.ts
+++ convex/projects.ts
@@ -1,6 +1,16 @@
 import { mutation, query } from './_generated/server'
 import { v } from 'convex/values'
 
+function isValidS3ImageUrl(url: string): boolean {
+  if (!url) return false
+  try {
+    const urlObj = new URL(url)
+    return urlObj.hostname === 'aok-projects-images.s3.us-east-2.amazonaws.com'
+  } catch {
+    return false
+  }
+}
+
 export const getAllProjects = query({
   handler: async (ctx) => {
     const projects = await ctx.db.query('projects').collect()
@@ -77,5 +87,39 @@ export const checkImageUrls = query({
       imageUrl: project.imageUrl,
       images: project.images,
     }))
+  },
+})
+
+export const get = query({
+  handler: async (ctx) => {
+    const projects = await ctx.db.query('projects').collect()
+    return projects.map((project) => ({
+      ...project,
+      // Make sure imageUrl is being returned and is a full S3 URL
+      imageUrl: project.imageUrl, // Should be the full S3 URL
+    }))
+  },
+})
+
+export const fixBrokenImageUrls = mutation({
+  handler: async (ctx) => {
+    const projects = await ctx.db.query('projects').collect()
+
+    let fixed = 0
+    for (const project of projects) {
+      if (!isValidS3ImageUrl(project.imageUrl)) {
+        // You could either:
+        // 1. Set a default image
+        await ctx.db.patch(project._id, {
+          imageUrl:
+            'https://aok-projects-images.s3.us-east-2.amazonaws.com/default-project-image.jpg',
+        })
+        // 2. Or set to null
+        // await ctx.db.patch(project._id, { imageUrl: null });
+        fixed++
+      }
+    }
+
+    return { fixed }
   },
 })
