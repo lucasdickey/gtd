ff423c162644c3caee3cab4d3dbc8e8177f86b4a
Lucas Dickey <lucas@lucasdickey.com>
2024-11-30 12:10:54 -0800

    Update calendar.tsx

--- components/ui/calendar.tsx
+++ components/ui/calendar.tsx
@@ -51,12 +51,10 @@ function Calendar({
         day_hidden: 'invisible',
         ...classNames,
       }}
-      components={
-        {
-          IconLeft: () => <ChevronLeft className="h-4 w-4" />,
-          IconRight: () => <ChevronRight className="h-4 w-4" />,
-        } satisfies Partial<DayPickerDefaultProps['components']>
-      }
+      components={{
+        IconLeft: () => <ChevronLeft className="h-4 w-4" />,
+        IconRight: () => <ChevronRight className="h-4 w-4" />,
+      }}
       {...props}
     />
   )
